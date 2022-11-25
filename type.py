2fa17d87bb26f1709cf0894b6ddc3314badce892
sumitLovesPython <118156583+sumitXcode@users.noreply.github.com>
2022-11-25 19:18:57 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,57 @@
+from random_word import RandomWords
+import time
+import readchar
+
+# Function to convert seconds to minutes
+def sec_to_min(secs):
+    m, s = divmod(secs, 60)
+    return m + (s / 60)
+
+
+# Initializing the words using 'random_word' module
+def Words():
+    r = RandomWords()
+    global length, words
+    for i in range(5):
+        word = r.get_random_word()
+        words += word + " "
+    word = r.get_random_word()
+    words += word
+    print(words)
+
+
+# To calculate the typing speed in words per minute 'WPM'
+def wpm():
+    global words, errors, begin, end
+    return round(((len(words) - errors)/ 5) / (sec_to_min(end - begin)))
+
+
+# Calculating the accuracy
+def accuracy():
+    global words
+    myWord = ""
+    errors = 0
+    while myWord != words:
+        key = readchar.readchar()
+        if ord(key) == 127:
+            print("\033[1D", end="", flush=True)
+            print(" ", end="", flush=True)
+            print("\033[1D", end="", flush=True)
+            myWord = myWord[:-1]
+        else:
+            if key != words[len(myWord)]:
+                errors += 1
+            myWord += key
+            print(key, end="", flush=True)
+    acc = (len(words) - errors) / len(words) * 100
+    print("\n")
+    print("Accuracy : ", round(acc), "%", sep="") if acc >= 0 else print("Accuracy:0%")
+
+
+errors = 0
+words = ""
+Words()
+begin = time.time()
+accuracy()
+end = time.time()
+print("\nSpeed    : ", wpm(), "wpm\n", sep="")
