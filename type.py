266ebda44b2886b596898447379b27459794ea81
sumitLovesPython <118156583+sumitXcoder@users.noreply.github.com>
2022-11-26 12:30:22 +0530

    Update type.py

--- type.py
+++ type.py
@@ -11,19 +11,17 @@ def sec_to_min(secs):
 # Initializing the words using 'random_word' module
 def Words():
     r = RandomWords()
-    global length, words
-    for i in range(5):
-        word = r.get_random_word()
-        words += word + " "
-    word = r.get_random_word()
-    words += word
+    global words
+    for i in range(4):
+        words += r.get_random_word() + " "
+    words += r.get_random_word()
     print(words)
 
 
 # To calculate the typing speed in words per minute 'WPM'
 def wpm():
     global words, errors, begin, end
-    return round(((len(words) - errors)/ 5) / (sec_to_min(end - begin)))
+    return round(((len(words) - errors) / 5) / (sec_to_min(end - begin)))
 
 
 # Calculating the accuracy
@@ -34,18 +32,23 @@ def accuracy():
     while myWord != words:
         key = readchar.readchar()
         if ord(key) == 127:
-            print("\033[1D", end="", flush=True)
-            print(" ", end="", flush=True)
-            print("\033[1D", end="", flush=True)
+            print("\033[1D \033[1D", end="", flush=True)
             myWord = myWord[:-1]
         else:
-            if key != words[len(myWord)]:
-                errors += 1
+            if len(myWord) < len(words):
+                if key != words[len(myWord)]:
+                    errors += 1
+                    print("\033[91m", key, "\033[00m", end="", sep="", flush=True)
+                else:
+                    print(key, end="", flush=True)
+            else:
+                #printing the key in 'red' color when its an error
+                print("\033[91m", key, "\033[00m", end="", sep="", flush=True)
             myWord += key
-            print(key, end="", flush=True)
+
     acc = (len(words) - errors) / len(words) * 100
     print("\n")
-    print("Accuracy : ", round(acc), "%", sep="") if acc >= 0 else print("Accuracy:0%")
+    print("Accuracy : ", round(acc), "%", sep="") if acc >= 0 else print("Accuracy :0%")
 
 
 errors = 0
